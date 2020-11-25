f2f40d1e40ec0ed0884c03d83b39d73d2285588a
PERNICENI Ugo <ugo.perniceni@hotmail.fr>
2020-11-25 23:31:46 +0100

    update default selection of language :construction:

--- app/src/main/java/fr/esgi/cocotton/SettingFragment.kt
+++ app/src/main/java/fr/esgi/cocotton/SettingFragment.kt
@@ -1,5 +1,6 @@
 package fr.esgi.cocotton
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +10,6 @@ import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.fragment.app.Fragment
-import kotlin.collections.ArrayList
 
 
 /**
@@ -19,7 +19,8 @@ import kotlin.collections.ArrayList
  */
 class SettingFragment : Fragment() {
 
-    private var spinner: Spinner? = null
+    private var spinnerLanguages: Spinner? = null
+    private var spinnerThemes: Spinner? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -30,13 +31,16 @@ class SettingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        spinner = view.findViewById(R.id.spinner_language)
+        spinnerLanguages = view.findViewById(R.id.spinner_language)
+        spinnerThemes = view.findViewById(R.id.spinner_theme)
 
-        initializeSpinner()
+        initializeSpinners()
     }
 
-    private fun initializeSpinner(){
-        spinner?.apply {
+    private fun initializeSpinners(){
+        val sharedPreferences = this.activity?.getSharedPreferences("Settings", Activity.MODE_PRIVATE)
+
+        spinnerLanguages?.apply {
             val languagesArray = resources.getStringArray(R.array.languages_array)
             val adapter: ArrayAdapter<String> = ArrayAdapter(context, android.R.layout.simple_spinner_item, languagesArray)
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -45,15 +49,38 @@ class SettingFragment : Fragment() {
             this.onItemSelectedListener = object : OnItemSelectedListener {
                 override fun onItemSelected(adapterView: AdapterView<*>?, view: View, position: Int, l: Long) {
                     when (position) {
-                        1 -> (activity as MainActivity?)?.setLocale("eng")
+                        0 -> (activity as MainActivity?)?.setLocale("eng")
+                        1 -> (activity as MainActivity?)?.setLocale("fr")
                         2 -> (activity as MainActivity?)?.setLocale("es")
-                        3 -> (activity as MainActivity?)?.setLocale("fr")
                         else -> {}
                     }
                 }
                 override fun onNothingSelected(adapterView: AdapterView<*>?) {}
             }
+            val language = sharedPreferences?.getString("language", "eng")
+
+            if(languagesInteger().containsKey(language)){
+                languagesInteger()[language]?.let  { it1 -> this.setSelection(it1) }
+            }else {
+                this.setSelection(1)
+            }
         }
+
+        spinnerThemes?.apply {
+            val themesArray = resources.getStringArray(R.array.themes_array)
+            val adapter: ArrayAdapter<String> = ArrayAdapter(context, android.R.layout.simple_spinner_item, themesArray)
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            this.adapter = adapter
+        }
+    }
+
+    private fun languagesInteger(): HashMap<String, Int> {
+        val translations = hashMapOf<String, Int>()
+        translations["eng"] = 0
+        translations["fr"] = 1
+        translations["es"] = 2
+
+        return translations
     }
 
     companion object {
