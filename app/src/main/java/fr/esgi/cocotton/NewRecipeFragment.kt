3e1b97b5e6dc2ca5b95fb0a4f00266bf39389451
PERNICENI Ugo <ugo.perniceni@hotmail.fr>
2020-12-22 18:38:13 +0100

    start adding form's validation :sparkles:

--- app/src/main/java/fr/esgi/cocotton/NewRecipeFragment.kt
+++ app/src/main/java/fr/esgi/cocotton/NewRecipeFragment.kt
@@ -1,6 +1,9 @@
 package fr.esgi.cocotton
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,7 +11,6 @@ import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import fr.esgi.cocotton.model.Recipe
-import org.w3c.dom.Text
 
 /**
  * A simple [Fragment] subclass.
@@ -21,8 +23,11 @@ class NewRecipeFragment : Fragment(), View.OnClickListener {
     private var pickerHours: NumberPicker? = null
     private var pickerMinutes: NumberPicker? = null
 
-    private var textViewHours: TextView? = null;
+    private var textViewHours: TextView? = null
     private var textViewMinutes: TextView? = null
+
+    private var editTextName: EditText? = null
+    private var editTextPerson: EditText? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -42,6 +47,9 @@ class NewRecipeFragment : Fragment(), View.OnClickListener {
 
         textViewHours = view.findViewById(R.id.new_recipe_text_view_hours)
         textViewMinutes = view.findViewById(R.id.new_recipe_text_view_minutes)
+
+        editTextName = view.findViewById(R.id.new_recipe_form_name)
+        editTextPerson = view.findViewById(R.id.new_recipe_form_for_number)
 
         spinner?.apply {
             val difficultiesArray = resources.getStringArray(R.array.difficulty_array)
@@ -68,6 +76,18 @@ class NewRecipeFragment : Fragment(), View.OnClickListener {
                 setUpTextMinutes(newVal)
             }
         }
+
+        editTextName?.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(text: Editable?) {
+                val content = text.toString()
+                editTextName?.error = if (content.isBlank() || content.length <= 3) "Veuillez remplir ce champ (3 char+)" else null
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
     }
 
     private fun setUpTextHours(counter: Int) {
@@ -91,17 +111,44 @@ class NewRecipeFragment : Fragment(), View.OnClickListener {
         }
     }
 
+    private fun formIsValid(): Boolean {
+        var validateForm = true
+
+        editTextName?.let {
+            if(TextUtils.isEmpty(it.text.toString())){
+                it.error = "Veuillez remplir ce champ (3 char+)"
+                validateForm = false
+            }
+        }
+
+        editTextPerson?.let {
+            if(TextUtils.isEmpty(it.text.toString())){
+                it.error = "Veuillez remplir ce champ"
+                validateForm = false
+            }
+        }
+
+        return validateForm
+    }
+
     private fun createNewRecipe(){
-        val name = view?.findViewById<EditText>(R.id.new_recipe_form_name)
-        val forPerson = view?.findViewById<EditText>(R.id.new_recipe_form_for_number)
-        val difficulty = view?.findViewById<Spinner>(R.id.new_recipe_form_spinner_difficulty)
-        val hours = pickerHours?.value?.times(60)
-        val minutes = pickerMinutes?.value
-        val time = minutes?.let { hours?.plus(it) }
+        if(formIsValid()){
+            val name = view?.findViewById<EditText>(R.id.new_recipe_form_name)
+            val forPerson = view?.findViewById<EditText>(R.id.new_recipe_form_for_number)
+            val difficulty = view?.findViewById<Spinner>(R.id.new_recipe_form_spinner_difficulty)
+            val hours = pickerHours?.value?.times(60)
+            val minutes = pickerMinutes?.value
+            val time = minutes?.let { hours?.plus(it) }
 
-        val newRecipe = Recipe("${name?.text}", time?.toLong(), ("${forPerson?.text}").toLong(), "${difficulty?.selectedItem}", "/path")
+            val newRecipe = Recipe("${name?.text}", time?.toLong(), ("${forPerson?.text}").toLong(), "${difficulty?.selectedItem}", "/path")
 
-        newRecipe.saveToDb()
+            newRecipe.saveToDb()
+
+            findNavController().navigate(R.id.action_NewRecipeFragment_to_HomeFragment)
+
+        }else{
+            Toast.makeText(context, "Remplir les champs svp", Toast.LENGTH_SHORT).show()
+        }
     }
 
     companion object {
