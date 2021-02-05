6f4cd1160a8d61165270d0f9971e5178372b8750
Kelig MARTIN <kmartin@wedigital.garden>
2021-02-05 17:03:41 +0100

    add profile fragment

--- app/src/main/java/fr/esgi/cocotton/ProfileFragment.kt
+++ app/src/main/java/fr/esgi/cocotton/ProfileFragment.kt
@@ -5,12 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class ProfileFragment : Fragment() {
+
+    private var userName: TextView? = null
+    private var userEmail: TextView? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val user = Firebase.auth.currentUser
+
+        userName = view.findViewById(R.id.user_display_name)
+        userName?.text = user?.displayName
+
+        userEmail = view.findViewById(R.id.user_email)
+        userEmail?.text = user?.email
     }
 }
