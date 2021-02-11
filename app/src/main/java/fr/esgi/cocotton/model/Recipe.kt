df7cc89dea8adda198e1e3e4fe3cbe6d1ce721d9
Kelig MARTIN <kmartin@wedigital.garden>
2021-02-11 22:29:10 +0100

    update user icon id

--- app/src/main/java/fr/esgi/cocotton/model/Recipe.kt
+++ app/src/main/java/fr/esgi/cocotton/model/Recipe.kt
@@ -1,10 +1,7 @@
 package fr.esgi.cocotton.model
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 
 data class Recipe(val name: String, val time: Long? =  0, val forPerson: Long =  0, val difficulty: String  =  "", val icon: Number = 0, val ingredients: List<Ingredient>, val steps: String = "", val authorDisplayName: String = "", val authorEmail: String = "") {
-    @PrimaryKey(autoGenerate = true)
     var id: Int = 0
 
     fun saveToDb(){
